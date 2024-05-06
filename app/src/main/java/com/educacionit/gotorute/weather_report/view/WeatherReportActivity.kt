64fd8b304ebc47d3b21eb14c2de52d48f5e9a5b1
Jonathan Aranguri <arangurim@gmail.com>
2024-05-06 01:33:26 -0300

    feat: Loading weather page with animation

--- app/src/main/java/com/educacionit/gotorute/weather_report/view/WeatherReportActivity.kt
+++ app/src/main/java/com/educacionit/gotorute/weather_report/view/WeatherReportActivity.kt
@@ -4,21 +4,21 @@ import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.activity.enableEdgeToEdge
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.educacionit.gotorute.R
-import com.google.android.material.progressindicator.CircularProgressIndicator
 
 class WeatherReportActivity : AppCompatActivity() {
 
     private lateinit var weatherWebview: WebView
-    private lateinit var loadingView: CircularProgressIndicator
+    private lateinit var loadingView: ProgressBar
     private lateinit var toolbar: Toolbar
+    private var isFirstLoad = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +38,7 @@ class WeatherReportActivity : AppCompatActivity() {
 
     @SuppressLint("SetJavaScriptEnabled")
     private fun configureWebview() {
+        weatherWebview.visibility = View.INVISIBLE
         weatherWebview.settings.javaScriptEnabled = true
         weatherWebview.webViewClient = object : WebViewClient() {
             override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
@@ -47,20 +48,39 @@ class WeatherReportActivity : AppCompatActivity() {
 
             override fun onPageFinished(view: WebView?, url: String?) {
                 super.onPageFinished(view, url)
-                loadingView.visibility = View.GONE
+                if (isFirstLoad) {
+                    loadPageWithAnimation()
+                }
             }
         }
         loadCustomUrl()
     }
 
+    private fun loadPageWithAnimation() {
+        val fadeInAnimation = AnimationUtils.loadAnimation(this@WeatherReportActivity, R.anim.fade_in)
+        fadeInAnimation.setAnimationListener(object : Animation.AnimationListener {
+
+            override fun onAnimationStart(animation: Animation?) {}
+
+            override fun onAnimationEnd(animation: Animation?) {
+                loadingView.visibility = View.GONE
+                weatherWebview.visibility = View.VISIBLE
+                isFirstLoad = false
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {}
+        })
+        weatherWebview.startAnimation(fadeInAnimation)
+    }
+
     private fun loadCustomUrl() {
         val latitude = intent.getStringExtra(LATITUDE_EXTRA)
         val longitude = intent.getStringExtra(LONGITUDE_EXTRA)
-        val weatherUrl= "https://windy.app/map/#c=$latitude,$longitude&z=30"
+        val weatherUrl = "https://windy.app/map/#c=$latitude,$longitude&z=30"
         weatherWebview.loadUrl(weatherUrl)
     }
 
-    companion object{
+    companion object {
         const val LATITUDE_EXTRA = "LATITUDE_EXTRA"
         const val LONGITUDE_EXTRA = "LONGITUDE_EXTRA"
     }
