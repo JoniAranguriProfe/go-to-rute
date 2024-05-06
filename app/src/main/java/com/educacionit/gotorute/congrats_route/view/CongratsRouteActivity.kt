6c243bcfc9ebe79e90c0545283980abc8910e2ca
Jonathan Aranguri <arangurim@gmail.com>
2024-05-06 05:13:34 -0300

    feat: Adding congrats screen with lottie animation

---
+++ app/src/main/java/com/educacionit/gotorute/congrats_route/view/CongratsRouteActivity.kt
@@ -0,0 +1,32 @@
+package com.educacionit.gotorute.congrats_route.view
+
+import android.media.MediaPlayer
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.educacionit.gotorute.R
+
+class CongratsRouteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_congrats_route)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        playCongratsSound()
+    }
+
+    private fun playCongratsSound() {
+        val mediaPlayer = MediaPlayer.create(this, R.raw.applause)
+        mediaPlayer.start()
+    }
+}
